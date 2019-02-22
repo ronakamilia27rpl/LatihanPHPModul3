a17d9304e9ff69328b298107c591dd6a86d848d9
ronakamilia27rpl <46396999+ronakamilia27rpl@users.noreply.github.com>
2019-02-22 10:56:30 +0700

    selesai dikerjakan

---
+++ Latihan3.3.php
@@ -0,0 +1,9 @@
+<?php
+    $user = "Rona";
+    $pass = "123" ;
+    if ($user == "Rona" && $pass == "123") {
+        echo "Login Berhasil";
+    } else {
+        echo "Login Gagal";
+    }
+    ?>
